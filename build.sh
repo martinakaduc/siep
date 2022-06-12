3ba2d5ce5f96a7d97715068a8872a8368d7e05e7
Nguyễn Quang Đức <nguyenquangduc2000@gmail.com>
2022-06-12 22:46:45 +0700

    Fix build script

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-cd boostgraphql && mkdir objs && make
-cd ../BoostISO && mkdir objs && make
-cd ../boostquicksi && mkdir objs && make
-cd ../CECI && mkdir objs && make
-cd ../CECI-boost && mkdir objs && make
-cd ../cfl-boost && mkdir objs && make
-cd ../cfl-match && mkdir objs && make
-cd ../DAF && mkdir objs && make
-cd ../DAF-boost && mkdir objs && make
-cd ../graphql && mkdir objs && make
-cd ../quicksi && mkdir objs && make
-cd ../TurboISO && mkdir objs && make
-cd ../VF3 && mkdir objs && make
-cd ../VF3-boost && mkdir objs && make
+cd boostgraphql && rm -rf objs && mkdir objs && make
+cd ../BoostISO && rm -rf objs && mkdir objs && make
+cd ../boostquicksi && rm -rf objs && mkdir objs && make
+cd ../CECI && rm -rf objs && mkdir objs && make
+cd ../CECI-boost && rm -rf objs && mkdir objs && make
+cd ../cfl-boost && rm -rf objs && mkdir objs && make
+cd ../cfl-match && rm -rf objs && mkdir objs && make
+cd ../DAF && rm -rf objs && mkdir objs && make
+cd ../DAF-boost && rm -rf objs && mkdir objs && make
+cd ../graphql && rm -rf objs && mkdir objs && make
+cd ../quicksi && rm -rf objs && mkdir objs && make
+cd ../TurboISO && rm -rf objs && mkdir objs && make
+cd ../VF3 && rm -rf objs && mkdir objs && make
+cd ../VF3-boost && rm -rf objs && mkdir objs && make
