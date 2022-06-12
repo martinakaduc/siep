765af9efd51972f408105b342632b46070a5e3fb
Nguyễn Quang Đức <nguyenquangduc2000@gmail.com>
2022-06-12 21:28:20 +0700

    Add build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd boostgraphql & make
+cd ../BoostISO & make
+cd ../boostquicksi & make
+cd ../CECI & make
+cd ../CECI-boost & make
+cd ../cfl-boost & make
+cd ../cfl-match & make
+cd ../DAF & make
+cd ../DAF-boost & make
+cd ../graphql & make
+cd ../quicksi & make
+cd ../TurboISO & make
+cd ../VF3 & make
+cd ../VF3-boost & make
